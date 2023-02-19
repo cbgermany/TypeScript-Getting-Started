ec531323d83902f17cb8d2ee800241f616eb232d
Chris Germany <Chris.Germany@icloud.com>
2023-02-19 12:49:11 +0000

    updates

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,35 @@
 function startGame() {
   // starting a new game
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game ...';
+
+  const playerName: string | undefined = getInputValue('playername');
+  logPlayer(playerName);
+
+  postScore(100, playerName);
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+  console.log(`The player is called: ${name}`)
+}
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = document.getElementById(elementID) as HTMLInputElement;
+
+  if (inputElement.value === '' ) {
+    return undefined
+  }
+  else {
+    return inputElement.value
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+logMessage("Welcome to MultiMath");
+
